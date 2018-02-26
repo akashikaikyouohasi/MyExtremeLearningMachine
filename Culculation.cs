0471d7e5c621e373ec6aba49eb46defb49b9a38a
aniki <yu2345678aniki@gmail.com>
2018-02-26 23:13:29 +0900

    必要な計算を並べた

--- Culculation.cs
+++ Culculation.cs
@@ -6,68 +6,33 @@ using System.Threading.Tasks;
 
 namespace MyExtremeLearningMachine
 {
-    public class Culculation
+    public class MatrixCulculation
     {
-        public void CulcMatrix_H_inv()
+        public void CulcInverseMatrix()
         {
             //List<List<double>> a = new List<List<double>>;
 
 
             /*
 	        matrix1を逆行列にしてmatrix2に代入する
-	        matrix1の行と列数L　正則行列！
 	        */
+            
+        }
 
-
+        public void CulcInnerProduct()
+        {
             /*
-            double buf; //一時的なデータを蓄える
-            int n = L;  //配列の次数
+             matrix1とmatrix2の内積を計算して、結果を返すメソッド
+             */
 
-            //単位行列を作る
-            //#pragma omp parallel num_threads(procs)
-            //{
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    matrix2[i][j] = (i == j) ? 1.0 : 0.0;
-                }
-            }
-            //}
+        }
 
-            //掃き出し法
-            for (int i = 0; i < n; i++)
-            {
-                buf = 1 / matrix1[i][i];
-                for (int j = 0; j < n; j++)
-                {
-                    matrix1[i][j] *= buf;
-                    matrix2[i][j] *= buf;
-                }
-                for (int j = 0; j < n; j++)
-                {
-                    if (i != j)
-                    {
-                        buf = matrix1[j][i];
-                        for (int k = 0; k < n; k++)
-                        {
-                            matrix1[j][k] -= matrix1[i][k] * buf;
-                            matrix2[j][k] -= matrix2[i][k] * buf;
-                        }
-                    }
-                }
-            }
-            */
-
+        public void CulcSigmoid()
+        {
             /*
-            //逆行列を出力
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    cout << matrix2[i][j] << " ";
-                }
-                cout << endl;
-            }
-            */
+             * シグモイド関数に代入して計算した結果を返すメソッド
+             * */
+
 
         }
 
