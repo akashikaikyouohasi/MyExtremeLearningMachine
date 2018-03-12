625789405270edb56c66758a9d0cd952295ab7d1
hayatata <hayatata@live.jp>
2018-03-13 00:03:53 +0900

    CulcInnerProductメソッドにテストパターンを１つ追加したところ、テストがレッドになってしまった。 これは、TDDの三角測量。先ほどのテストのグリーンはたまたま通っただけであることがわかった。次は、２つのパターンのテストを通すように実装し、リファクタリングを行う。

--- UnitTest_MyExtreamLearningTest/UnitTest_Culclation.cs
+++ UnitTest_MyExtreamLearningTest/UnitTest_Culclation.cs
@@ -26,13 +26,25 @@ namespace UnitTest_MyExtremLearningTest
         {
             var frm = new EigenFuncs();
 
+            //テストパターン１
             float[,] mat1 = new float[2, 2] { { 2f, -3f }, { -1f, 4f } };
             float[,] mat2 = new float[2, 1] { { 1f }, { 2f } };
             float[,] result = new float[2,1];
-            float[,] arr = new float[2,1] { { 0f }, { 5f } };
+            float[,] answer = new float[2,1] { { 0f }, { 5f } };
 
             result = frm.CulcInnerProduct(mat1, mat2);
-            CollectionAssert.AreEqual(arr, result);
+            CollectionAssert.AreEqual(answer, result);
+
+            //テストパターン２
+            mat1 = new float[1, 3] { { 1f, 5f, 9f }};
+            mat2 = new float[3, 3] { { 8f, 1f, -7f }, { 4f, 3f, 0f } , { 2f, -6f, 5f } };
+            result = new float[1, 3];
+            answer = new float[1, 3] { { -50f, 19f, 17f } };
+
+            result = frm.CulcInnerProduct(mat1, mat2);
+            CollectionAssert.AreEqual(answer, result);
+
+
         }
 
     }
